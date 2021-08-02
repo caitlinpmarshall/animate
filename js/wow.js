765bc4c6851a92eb8e5242f626ac0abb7a47f749
caitlinpmarshall <caitlinpmarshall@gmail.com>
2021-08-02 09:51:11 -0400

    Playing around with some basic JS before implementing wow library

---
+++ js/wow.js
@@ -0,0 +1,7 @@
+const myHeader = document.querySelector(".myHeader");
+const paraOne = document.getElementById("paraOne");
+
+console.log(myHeader);
+console.log(paraOne);
+
+paraOne.className.add("big");
